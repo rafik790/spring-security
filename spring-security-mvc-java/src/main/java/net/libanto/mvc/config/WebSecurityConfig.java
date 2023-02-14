60f6d76d557630c47b1eca1b8456df8fc9d610f0
Rafik Mondal <RafikM@interrait.com>
2023-02-14 23:31:48 +0530

    Spring Security Login-Logout Module

--- spring-security-mvc-java/src/main/java/net/libanto/mvc/config/WebSecurityConfig.java
+++ spring-security-mvc-java/src/main/java/net/libanto/mvc/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package net.libanto.mvc.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -9,17 +10,26 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
+@ComponentScan({ "net.libanto.mvc.controller" })  
 public class WebSecurityConfig implements WebMvcConfigurer {
 	@Bean
 	public UserDetailsService userDetailsService() throws Exception {
 		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 		manager.createUser(
-				User.withDefaultPasswordEncoder().username("javatpoint").password("java123").roles("USER").build());
+				User.withDefaultPasswordEncoder().username("javatpoint").password("java123").roles("ADMIN").build());
 		return manager;
 	}
 	
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.antMatcher("/").authorizeRequests().anyRequest().hasRole("ADMIN").and().httpBasic();
+		 http                              
+	        .authorizeRequests()  
+	            .anyRequest().hasRole("ADMIN")  
+	            .and().formLogin().and()  
+	        .httpBasic()  
+	        .and()  
+	        .logout()  
+	        .logoutUrl("/j_spring_security_logout")  
+	        .logoutSuccessUrl("/") ;  
 	}
 }
--- spring-security-mvc-java/src/main/webapp/WEB-INF/views/privatePage.jsp
+++ spring-security-mvc-java/src/main/webapp/WEB-INF/views/privatePage.jsp
@@ -1,8 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"   
+"http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Admin</title>
 </head>
-<body>Hello Admin
+<body>
+	Hello Admin
+	<div>
+		<a href="<c:url value='/logout' />">Click here to logout</a>
+	</div>
 </body>
 </html>
